bea9d2f5ca56a1f80a7e6e4a226e03e5671acad9
unknown <jjy963851@gmail.com>
2023-06-26 19:22:39 -0400

    wow

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ return(
 <>
 <header className="">
 
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center headerFont">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
    <Link href ="/" legacyBehavior>
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
