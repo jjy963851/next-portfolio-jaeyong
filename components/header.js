19952038a1e277e7bd64f93d15dac106fbdf7dac
unknown <jjy963851@gmail.com>
2023-06-26 18:48:17 -0400

    push

--- components/header.js
+++ components/header.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Header(){
 return(
 
-<div className="dark:headerFont">
-<header className="text-gray-600 body-font dark:headerFont">
+<>
+<header className="headerFont ">
 
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
    <Link href ="/" legacyBehavior>
@@ -13,7 +13,7 @@ return(
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span className="ml-3 text-xl dark:headerFont">
+        <span className="ml-3 text-xl headerFont">
           JaeYong&apos;s Portfolio
           </span>
         </a>
@@ -22,19 +22,19 @@ return(
     
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <Link href = "/" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900 dark:text-white">Home</a>
+        <a className="mr-5 hover:text-gray-900 ">Home</a>
       </Link>
       <Link href = "/Projects" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900 dark:text-white">Projects</a>
+        <a className="mr-5 hover:text-gray-900 ">Projects</a>
       </Link>
       <Link href = "https://github.com/jjy963851" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900 dark:text-white">Github</a>
+        <a className="mr-5 hover:text-gray-900 ">Github</a>
       </Link>
     </nav>
   
   </div>
 </header>
-</div>
+</>
 
 
 
