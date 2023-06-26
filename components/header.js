c333d7c8478b7325477576026fe8b6c134438239
unknown <jjy963851@gmail.com>
2023-06-26 18:17:15 -0400

    header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header(){
 return(
 <>
-<header className="text-gray-600 body-font">
+<header className="text-gray-600 body-font .header">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
    <Link href ="/" legacyBehavior>
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
