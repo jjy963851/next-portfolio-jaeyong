1038249ac362d2a5f84231f08cf8e20fd00c195b
unknown <jjy963851@gmail.com>
2023-06-26 18:09:59 -0400

    font color

--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ return(
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span className="ml-3 text-xl">
+        <span className="ml-3 text-xl dark:text-white">
           JaeYong&apos;s Portfolio
           </span>
         </a>
@@ -20,13 +20,13 @@ return(
     
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <Link href = "/" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900">Home</a>
+        <a className="mr-5 hover:text-gray-900 dark:text-white">Home</a>
       </Link>
       <Link href = "/Projects" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900">Projects</a>
+        <a className="mr-5 hover:text-gray-900 dark:text-white">Projects</a>
       </Link>
       <Link href = "https://github.com/jjy963851" legacyBehavior>
-        <a className="mr-5 hover:text-gray-900">Github</a>
+        <a className="mr-5 hover:text-gray-900 dark:text-white">Github</a>
       </Link>
     </nav>
   
--- components/home/hero.js
+++ components/home/hero.js
@@ -4,12 +4,12 @@ export default function Hero(){
     return(
       <>
        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center ">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium ">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium dark:text-white ">
         Hello Everyone! I&apos;m JaeYong
-        <br className="hidden lg:inline-block"/>
+        <br className="hidden lg:inline-block dark:text-white "/>
         Welcome to my Portfolio Website!
       </h1>
-      <p className="mb-8 leading-relaxed">
+      <p className="mb-8 leading-relaxed dark:text-white ">
         Hello, everyone. Thanks for watching my Portfolio. I&apos;m currently studying in La Roche University. I&apos;m majoring Computer Scienece, and my goal is to become full stack developer! I hope you like my projects and if you want to take a look more details about my projects, please visit my github! Thank you so much.
         </p>
       <div className="flex justify-center">
