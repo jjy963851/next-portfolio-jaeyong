a1d71b89a8e98ac532aa2fa7e8ec5a08279149c7
unknown <jjy963851@gmail.com>
2023-06-26 18:36:58 -0400

    fixing

--- components/header.js
+++ components/header.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Header(){
 return(
 
-<div className="headerFont">
-<header className="text-gray-600 body-font headerFont">
+<div className="dark:headerFont">
+<header className="text-gray-600 body-font dark:headerFont">
 
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
    <Link href ="/" legacyBehavior>
@@ -13,7 +13,7 @@ return(
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span className="ml-3 text-xl headerFont">
+        <span className="ml-3 text-xl dark:headerFont">
           JaeYong&apos;s Portfolio
           </span>
         </a>
